08f3e9101e2594572c438b68959204e61c1e6be5
Jade Doucet <jadejdoucet@gmail.com>
2025-03-01 22:52:19 -0800

    remove scroll to top button

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,12 @@
-import React, { PropsWithChildren, useEffect, useState } from 'react';
-import { Container, Text, Button } from '@mantine/core';
+import React, { PropsWithChildren } from 'react';
+import { Container, Text } from '@mantine/core';
 
 import NavItem from '../NavItem';
+import Starfield from '../Starfield';
+import ShootingStars from '../ShootingStars';
 import { ENavigationOptions, EPage } from '../../types';
 
 import styles from './Layout.module.css';
-import Starfield from '../Starfield';
-import ShootingStars from '../ShootingStars';
 
 interface LayoutProps extends PropsWithChildren {
   currentPage: EPage;
@@ -14,35 +14,6 @@ interface LayoutProps extends PropsWithChildren {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children, currentPage, navigateTo }) => {
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-  };
-
-  // Take the image I just downloaded with the mountains, split the second mountains to create parallax
-  const [showScrollButton, setShowScrollButton] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      // Check if the user has scrolled down at least 200 pixels
-      if (window.scrollY > 200) {
-        setShowScrollButton(true);
-      } else {
-        setShowScrollButton(false);
-      }
-    };
-
-    // Attach the scroll event listener when the component mounts
-    window.addEventListener('scroll', handleScroll);
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
     <Container
       fluid
@@ -81,11 +52,6 @@ const Layout: React.FC<LayoutProps> = ({ children, currentPage, navigateTo }) =>
       </header>
       <main>{children}</main>
       <footer className={styles.footer}>
-        {showScrollButton && (
-          <Button size="sm" variant="gradient" my={10} onClick={scrollToTop}>
-            Back to Top
-          </Button>
-        )}
         <Text size="sm">
           © {new Date().getFullYear()} <i>JD Development & Design</i>
         </Text>
